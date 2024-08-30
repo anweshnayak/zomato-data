30736542afcefc4c41fa852b9cda2025184e30a2
anweshnayak <81944911+anweshnayak@users.noreply.github.com>
2024-08-30 13:51:46 +0530

    Delete project1.py

--- project1.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-df = pd.read_csv("Expanded_data_with_more_features.csv")
-print (df.head())
-
-df.describe()
